7b179a3362520980aed7745d062ee47081ceddd3
りんご飴196 <kazumac41@gmail.com>
2024-03-09 20:28:08 +0900

    カスタムモデルデータも追加

--- src/main/kotlin/com/github/AoRingoServer/ItemManager.kt
+++ src/main/kotlin/com/github/AoRingoServer/ItemManager.kt
@@ -4,11 +4,12 @@ import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 
 class ItemManager {
-    fun make(itemType:Material,displayName:String,lore:MutableList<String>? = null,amount:Int = 1):ItemStack{
+    fun make(itemType:Material,displayName:String,lore:MutableList<String>? = null,amount:Int = 1,customModelData:Int? = null):ItemStack{
         val item = ItemStack(itemType,amount)
         val meta = item.itemMeta
         meta?.setDisplayName(displayName)
         meta?.lore = lore
+        meta?.setCustomModelData(customModelData)
         item.setItemMeta(meta)
         return item
     }
