7b0fcf74c9b44543bee6be0bbe91c8a048e768a0
りんご飴196 <kazumac41@gmail.com>
2024-03-15 22:17:16 +0900

    [実装] 焼く系の処理を別クラスに作成 汎用性 高く

---
+++ src/main/kotlin/com/github/AoRingoServer/CookGame/Cookwares/Cookware.kt
@@ -0,0 +1,43 @@
+package com.github.AoRingoServer.CookGame.Cookwares
+
+import com.github.AoRingoServer.CookGame.FoodManager
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.entity.ItemFrame
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitRunnable
+
+class Cookware(private val plugin: Plugin) {
+    private val foodManager = FoodManager(plugin)
+    fun bakeItemFrameCooking(itemFrame: ItemFrame, food: ItemStack, player: Player, completionTime: Int, overTime: Int, bakeSound: Sound) {
+        val completionGoodsItem = foodManager.acquisitionCookingCompletionGoodsData(food, "fly") ?: return
+        val completionSound = Sound.BLOCK_ANVIL_USE
+        val burnedSound = Sound.BLOCK_LAVA_EXTINGUISH
+        var time = 0
+        object : BukkitRunnable() {
+            override fun run() {
+                val itemFrameItem = itemFrame.item
+                player.playSound(player, bakeSound, 1f, 1f)
+                time ++
+                if (itemFrameItem != food && itemFrameItem != completionGoodsItem) {
+                    this.cancel()
+                    return
+                }
+                when (time) {
+                    completionTime -> changeItemFrameItem(itemFrame, completionGoodsItem, completionSound, player)
+                    overTime -> {
+                        this.cancel()
+                        val airItem = ItemStack(Material.AIR)
+                        changeItemFrameItem(itemFrame, airItem, burnedSound, player)
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 20L) // 1秒間隔 (20 ticks) でタスクを実行
+    }
+    private fun changeItemFrameItem(itemFrame: ItemFrame, setItem: ItemStack, installationSound: Sound, player: Player) {
+        itemFrame.setItem(setItem)
+        player.playSound(player, installationSound, 1f, 1f)
+    }
+}
